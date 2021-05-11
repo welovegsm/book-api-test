a702f185ffb7b1077e37b9e43271e165881d4d3d
welovegsm <admin1@welovegsm.com>
2021-05-11 14:08:22 +0200

    Ajout message d'accuile

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 // On importe la libraire fastify
 import fastify from 'fastify'
+import fastifyMongo from 'fastify-mongodb'
 
 // On créé une application fastify en utilisant
 // L'import de notre librairie. On configure
 // fastify pour afficher des logs
 const app = fastify({ logger: true })
+// on Connect 
 
 // on cree une toute fastify sur l"url "/"
 app.get('/', async ()=>{
     return { text : "Welcome"}
 })
-
+// creation de la route qui retourn 
 // Cette fonction démarre notre server d'api
 const start = async () => {
   console.log('Lancement de notre server ...')
