36ed268e2471926b7af4745843c82c724806af58
welovegsm <admin1@welovegsm.com>
2021-05-11 15:57:58 +0200

    api

--- src/index.js
+++ src/index.js
@@ -6,33 +6,52 @@ import fastifyMongo from 'fastify-mongodb'
 // L'import de notre librairie. On configure
 // fastify pour afficher des logs
 const app = fastify({ logger: true })
+
 // On connécte la base de données MongoDB
 app.register(fastifyMongo, {
-    url: 'mongodb+srv://MyTodoApp:MyTodoApp@cluster0.obacx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-  })
+  url: 'mongodb+srv://MyTodoApp:MyTodoApp@cluster0.obacx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+})
 
-  // On créé une route qui retourne tout les livres de notre
+// On créé une route fastify sur l'URI "/"
+app.get('/', async () => {
+  return { text: "Welcome !" }
+})
+
+// On créé une route qui retourne tout les livres de notre
 // base de données (MongoDB)
 app.get('/books', async () => {
-    // Mongo est une base de données qui contient des collection
-    // (c'est un peu comme des tables en SQL)
-    // Ici on récupére la collection "books"
-    const collection = app.mongo.db.collection('books')
-  
-    // Sur cette collection nous pouvons utiliser plusieurs
-    // fonctions. Ici, nous allons récupérer TOUT les livres
-    const books = await collection.find().toArray()
-  
-    // Nous retournons tout les livres de la base de données
-    return books
-  })
-  
+  // Mongo est une base de données qui contient des collection
+  // (c'est un peu comme des tables en SQL)
+  // Ici on récupére la collection "books"
+  const collection = app.mongo.db.collection('books')
 
-// on cree une toute fastify sur l"url "/"
-app.get('/', async ()=>{
-    return { text : "Welcome"}
+  // Sur cette collection nous pouvons utiliser plusieurs
+  // fonctions. Ici, nous allons récupérer TOUT les livres
+  const books = await collection.find().toArray()
+
+  // Nous retournons tout les livres de la base de données
+  return books
 })
-// creation de la route qui retourn 
+
+// On créé un route qui nous permettra d'ajouter (de créer) un
+// nouveau livre
+app.post('/books', async (request) => {
+  // Nous récupérons toutes les données qu'il y a dans le corp
+  // d'une requête. Cela correspond à notre livre
+  const book = request.body
+
+  // Pour enregistrer le livre dans ma BDD (mongodb)
+  // j'ai besoin de la collection
+  const collection = app.mongo.db.collection('books')
+
+  // On enregistre le livre dans la base de données
+  const result = await collection.insertOne(book)
+
+  // On retourne le livre qui a été enregistré dans la base de
+  // données
+  return result.ops[0]
+})
+
 // Cette fonction démarre notre server d'api
 const start = async () => {
   console.log('Lancement de notre server ...')
